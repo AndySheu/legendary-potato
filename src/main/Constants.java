f9637b9c09e6ab2267d21ba6ab9a19af6c0dc6ca
Andy Sheu <clubcharger@gmail.com>
2017-05-08 10:21:10 -0700

    tweaked constants

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -3,10 +3,10 @@ package main;
 public class Constants {
 	static final int STARTING_PEST_COUNT = 10000;
 	static final int FORMATION_MUTATION_RATE = 400; // out of 1000 per digit
-	static final int RADIACTIVE_MUTATION_RATE = 400; // out of 1000000 per digit
+	static final int RADIACTIVE_MUTATION_RATE = 1000; // out of 1000000 per digit
 	static final int RESEARCH_TIME = 5;
 //	static final int RESEARCHER_PEST_SAMPLE_SIZE = 1000;
-	static final int RESEARCHER_PEST_SAMPLE_RATIO = 5;
+	static final int RESEARCHER_PEST_SAMPLE_RATIO = 3;
 	static final int DNA_LENGTH = 5;
 	static final int TARGET_DNA_LENGTH = 2;
 	static final int MUTATION_INTENSITY = 50;
